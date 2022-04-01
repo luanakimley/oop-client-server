b71082914d44668ebad2a5665c48424139fcd9a1
Luana Kimley <luanakimley@gmail.com>
2022-04-01 19:22:05 +0100

    JUnit tests for findPlayerById(), addPlayer(), deletePlayerById(), and findPlayerByNationality()

--- src/test/java/dkit/oop/AppTest.java
+++ src/test/java/dkit/oop/AppTest.java
@@ -1,8 +1,16 @@
 package dkit.oop;
 
-import static org.junit.Assert.assertTrue;
-
+import dkit.oop.DAOs.MySqlPlayerDAO;
+import dkit.oop.DTOs.Player;
+import dkit.oop.DTOs.Sector;
+import dkit.oop.Exceptions.DAOException;
 import org.junit.Test;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.*;
 
 /**
  * Unit test for simple App.
@@ -12,9 +20,95 @@ public class AppTest
     /**
      * Rigorous Test :-)
      */
+    MySqlPlayerDAO IPlayerDAO = new MySqlPlayerDAO();
+
     @Test
-    public void shouldAnswerWithTrue()
+    public void findPlayerByIdTest()
     {
-        assertTrue( true );
+        try
+        {
+            System.out.println("Find player by ID DAO test");
+
+            Player p = IPlayerDAO.findPlayerById(2);
+
+            assertEquals(p.getId(), 2);
+        }
+        catch (DAOException e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void addPlayerTest()
+    {
+        try
+        {
+            System.out.println("Add player DAO test");
+
+            int prevSize = IPlayerDAO.findAllPlayers().size();
+
+            Player p = new Player("Luana Kimley", "Indonesia", 2002, 8, 27, 1.62, Sector.MIXED_DOUBLES, 7);
+            IPlayerDAO.addPlayer(p);
+
+            Player newP = IPlayerDAO.findAllPlayers().get(prevSize);
+
+            assertEquals(prevSize + 1, IPlayerDAO.findAllPlayers().size());
+            assertEquals("Luana Kimley", newP.getName());
+            assertEquals("Indonesia", newP.getNationality());
+            assertEquals(LocalDate.of(2002,8,27), newP.getDateOfBirth());
+            assertEquals(1.62, newP.getHeight(), 0.01);
+            assertEquals(Sector.MIXED_DOUBLES, newP.getSector());
+            assertEquals(7, newP.getWorldRank());
+        }
+        catch (DAOException e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void deletePlayerByIdTest()
+    {
+        try
+        {
+            System.out.println("Delete player by ID DAO test");
+
+            int prevSize = IPlayerDAO.findAllPlayers().size();
+
+            IPlayerDAO.deletePlayerById(4);
+
+            Player p = IPlayerDAO.findPlayerById(4);
+
+            assertNull(p);
+            assertEquals(prevSize-1, IPlayerDAO.findAllPlayers().size());
+
+
+        }
+        catch (DAOException e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void findPlayerByNationalityTest()
+    {
+        try
+        {
+            System.out.println("Find player by nationality DAO test");
+
+            List<Player> playerList = IPlayerDAO.findPlayersByNationality("Denmark");
+
+            for (Player p : playerList)
+            {
+                assertEquals("Denmark", p.getNationality());
+            }
+
+        }
+        catch (DAOException e)
+        {
+            e.printStackTrace();
+        }
     }
 }
